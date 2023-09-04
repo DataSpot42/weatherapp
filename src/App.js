3d118d679145cfed7fe415365febb052a28153b4
DataSpot42 <stevelookingforward@gmail.com>
2023-09-04 23:11:55 +0100

    tidy up and bit more style

--- src/App.js
+++ src/App.js
@@ -1,117 +1,65 @@
 import { React, useState, useEffect, useContext, useRef } from "react"
-
 import { WeatherInfo1, WeatherInfo2, WeatherInfo3, WeatherInfo4 } from "./componants/weatherText"
 import PlaceComponent from "./componants/locationsearch";
-import { fadeIn } from "react-animations";
-import { StyleSheet, css } from 'aphrodite';
+import { motion } from "framer-motion";
+
 import './App.css'
-let isLoading=false  
+let isLoading = false
 const App = () => {
-  
-  
-  
   let [geo, setGeo] = useState("0");
-  const childToParent =(childData) => {
-  
+  const childToParent = (childData) => {
     setGeo(childData);
     console.log(geo);
-    
-    
- } 
-
-
-
-  
-  
+  }
   const defaulttown = 'London'
-  
-  const [location,setLocation] = useState([])
-  const [town,setTown] = useState('London')
-  const [weather,setWeather] = useState([{"name" : "."}])
-  const [mainTemp,SetMainTemp] = useState([{"temp" : "Awaiting Input"}])
-  const [chosenWeather,setChosenWeather] = useState('')
-  const [chosenWind,setChosenWind] = useState('')
-  const [chosenLocation,setChosenLocation] = useState('')
- 
-  
-  
-  
-/* const handlerLocationFind = async() => {
 
+  const [location, setLocation] = useState([])
+  const [town, setTown] = useState('London')
+  const [weather, setWeather] = useState([{ "name": "." }])
+  const [mainTemp, SetMainTemp] = useState([{ "temp": "Awaiting Input" }])
+  const [chosenWeather, setChosenWeather] = useState('')
+  const [chosenWind, setChosenWind] = useState('')
+  const [chosenLocation, setChosenLocation] = useState('')
 
-  console.log(town) 
-  
-  const API_KEY = process.env.REACT_APP_API_KEY 
-  
-  let response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${town}&limit=5&appid=${API_KEY}`)
- 
-  let data = await response.json()
-  console.log(data)
-  setLocation(data)
-  setTimeout(console.log(location),1000)
-  if (data.length == 0) {SetErrorCap(true); console.log('damn again'); setTown('London'); handleError()} else {errorthing=true}
-  setLocation(data)
-}  */
-  
-  const handlerFindWeather = async(geo) => {
-  console.log(geo)
-  const API_KEY = process.env.REACT_APP_API_KEY 
-  let response2 = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${geo[0]}&lon=${geo[1]}&units=metric&appid=${API_KEY}`)
-  let data2 = await response2.json()
-  setWeather(data2.weather)
-  SetMainTemp(data2.main)
-  setChosenLocation(data2)
-  setChosenWeather(data2.weather[0])
-  setChosenWind(data2.wind)
-  console.log(data2.main)
-  console.log(data2)
-  console.log(mainTemp)
-  isLoading=true
-  /* handlerFindWeather(geo) */
-  }  
- 
-
-/* const handleSubmit = (event) => {
-    event.preventDefault();    
-    console.log(town)    
-    handlerLocationFind()   
-    
-  }; */
-  const handleSelectedlocation = (geo,e) => {
-    e.preventDefault();
-    
+  const handlerFindWeather = async (geo) => {
     console.log(geo)
-   
-    
-    handlerFindWeather(geo)    
-  };  
+    const API_KEY = process.env.REACT_APP_API_KEY
+    let response2 = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${geo[0]}&lon=${geo[1]}&units=metric&appid=${API_KEY}`)
+    let data2 = await response2.json()
+    setWeather(data2.weather)
+    SetMainTemp(data2.main)
+    setChosenLocation(data2)
+    setChosenWeather(data2.weather[0])
+    setChosenWind(data2.wind)
+    console.log(data2.main)
+    console.log(data2)
+    console.log(mainTemp)
+    isLoading = true   
+  }
 
-   /* useEffect(() => {
+  const handleSelectedlocation = (geo, e) => {
+    e.preventDefault()
+    console.log(geo)
     handlerFindWeather(geo)
-  }, [])
-   */
+  };
+ 
   console.log(geo)
-  /* if (geo.length > 0) {console.log(`why??`); (e)=>handleSelectedlocation(geo,e.target.value)} */
-
-
   return (
-    <div class = "whole">
-      
+    <div class="whole">
       <h1>Steve's Weather App</h1>
-      
+      <PlaceComponent childToParent={childToParent} />
+      <button className="button" onClick={(e) => handlerFindWeather(geo, e.target.value)}>Get Weather</button>
+      {isLoading ?
         
-         <PlaceComponent childToParent={childToParent} />
-         <button class="getButton" onClick = {(e) => handlerFindWeather(geo,e.target.value)}>Get Weather</button>
-       {isLoading ?  
-       <div className="weatherBlock"> 
-       <h2><WeatherInfo1 {...mainTemp}/></h2>        
-      <h2><WeatherInfo3 {...chosenWeather}/></h2>      
-      <h2><WeatherInfo4 {...chosenWind}/></h2>  
-      <h2></h2>
-      <h2><WeatherInfo2 {...chosenLocation}{...isLoading}/></h2></div>
-      :<h2>Awaiting Input</h2>}
-      
-    </div>    
+        <div className="weatherBlock">
+          <h2><WeatherInfo1 {...mainTemp} /></h2>
+          <h2><WeatherInfo3 {...chosenWeather} /></h2>
+          <h2><WeatherInfo4 {...chosenWind} /></h2>
+          <h2></h2>
+          <h2><WeatherInfo2 {...chosenLocation}{...isLoading} /></h2></div>
+        : <h2>Awaiting Input</h2>}
+
+    </div>
   )
 }
 
--- src/componants/weatherText.js
+++ src/componants/weatherText.js
@@ -1,72 +1,80 @@
 import { useState, useEffect } from 'react'
 
-import WeatherIcon from 'react-icons-weather';
+
+
 import '../App.css'
 export function WeatherInfo1(props) {
     console.log(props)
     return (
         <div>
-            
+
             <div>
-                
-                
-                <h3>Max Temp: {Math.round(props.temp_max)} degC</h3>
-                <h3>Min Temp: {Math.round(props.temp_min)} degC</h3>
-                <h3>Feels Like: {Math.round(props.feels_like)} degC</h3>
-                <h3>Average Temp: {Math.round(props.temp)} degC</h3>             
+
+
+                <h3>Max Temp: {Math.round(props.temp_max)} °C</h3>
+                <h3>Min Temp: {Math.round(props.temp_min)} °C</h3>
+                <h3>Feels Like: {Math.round(props.feels_like)} °C</h3>
+                <h3>Average Temp: {Math.round(props.temp)} °C</h3>
             </div>
         </div>
     )
 
 }
 export function WeatherInfo2(props) {
-  console.log(props)
-  return (
-      <div>
-          
-          <div>
-              
-              <h3>{(props.name)}</h3>
-              
+    console.log(props)
+    return (
+        <div>
+
+            <div>
+
+                <h3>{(props.name)}</h3>
 
 
-          </div>
-      </div>
-  )
+
+            </div>
+        </div>
+    )
 
 }
 export function WeatherInfo3(props) {
-  console.log(props)
-  let icon = `http://openweathermap.org/img/w/${props.icon}.png`
-  console.log(icon)
-  return (
-      <div>
-          
-          <div>
-              
-              <h3>Weather: {(props.main)}</h3>
-              <h3>Weather Detail: {(props.description)}</h3>
-              <img src={icon} alt="Weather Icon" />
-                         
-          </div>
-      </div>
-  )
+    console.log(props)
+    let icon = `http://openweathermap.org/img/w/${props.icon}.png`
+    console.log(icon)
+    return (
+        <div>
+
+            <div>
+
+                <h3>Weather: {(props.main)}</h3>
+                <h3>Weather Detail: {(props.description)}</h3>
+                <img src={icon} alt="Weather Icon" width="100" height="100"/>
+
+            </div>
+        </div>
+    )
 
 }
 
 export function WeatherInfo4(props) {
-  console.log(props)
-  return (
-      <div>
-          
-          <div>
-              
-              <h3>Wind Speed: {(props.speed)}</h3>
-              <h3>Wind Drirection: {(props.deg)} deg</h3>
-                          
-          </div>
-      </div>
-  )
+    console.log(props)
+    let directions = ["N","NNE","NE","ENE","E",
+		"ESE", "SE", "SSE","S",
+		"SSW","SW","WSW","W",
+		"WNW","NW","NNW" ];
+    let section = parseInt( props.deg/22.5 + 0.5 );
+    section = section % 16;
+    
+    return (
+        <div>
+
+            <div>
+
+                <h3>Wind Speed: {(props.speed)}m/s</h3>
+                <h3>Wind Drirection: {(directions[section])}</h3>
+
+            </div>
+        </div>
+    )
 
 }
 //{<h4>More Data: {props.data4u}</h4>}
@@ -77,5 +85,4 @@ export function WeatherInfo4(props) {
  //   {props.map(title => {
    //     return <div key={title}>{title}</div>;
      // })}
-         
-    
+
